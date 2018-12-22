a37631adb29ddc097f2c1e0c1e739d23d8f78000
Kristiyan Bogdanov <kristiyan.r.bogdanov@gmail.com>
2018-12-22 15:24:52 +0000

    Build website

---
+++ Gemfile
@@ -0,0 +1,10 @@
+source "https://rubygems.org"
+
+group :jekyll_plugins do
+  gem 'jekyll-assets', '~> 3'
+  gem 'jekyll-feed', '~> 0.6'
+  gem 'jekyll-sitemap', '~> 1.1'
+  gem 'jekyll-paginate-v2'
+  gem 'jekyll-tagging'
+  gem 'jekyll-timeago'
+end
---
+++ _assets/javascript/app.js
@@ -0,0 +1,32 @@
+console.log("Welcome to my personal website!")
+
+// grab an element
+var myElement = document.querySelector(".header");
+// construct an instance of Headroom, passing the element
+var headroom  = new Headroom(myElement, {
+  "offset": 200,
+});
+// initialise
+headroom.init(); 
+
+
+// scroll reveal
+
+ScrollReveal().reveal('.design',{
+  delay: 0,
+  distance: '50px',
+  duration: 1000,
+  origin: "right"
+})
+ScrollReveal().reveal('.build',{
+  delay: 300,
+  distance: '50px',
+  duration: 1000,
+  origin: "right"
+})
+ScrollReveal().reveal('.ship',{
+  delay: 600,
+  distance: '50px',
+  duration: 1000,
+  origin: "right"
+})
